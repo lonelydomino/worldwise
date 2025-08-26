aec86d58cd1384985ac864e1231d06160cea9a3a
lonelydomino <lonelydomino@icloud.com>
2025-08-25 21:42:46 -0700

    Add Footer, Map, and Sidebar components, update App and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Product from "./pages/Product"
-import Pricing from "./pages/Pricing"
-import Homepage from "./pages/Homepage"
-import PageNotFound from "./pages/PageNotFound"
-import AppLayout from "./pages/AppLayout"
-import Login from "./pages/Login"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 // import PageNav from "./components/PageNav"
 
-
 function App() {
-
   return (
-  <div>
-    {/* <PageNav /> */}
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="pricing" element={<Pricing />} />
-      <Route path="product" element={<Product />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/app" element={<AppLayout />} />
-      <Route path="*" element={<PageNotFound />} />
+    <div>
+      {/* <PageNav /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Homepage />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="product" element={<Product />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/app" element={<AppLayout />}>
+            <Route index element={<p>List of cities</p>} />
+            <Route path="cities" element={<p>cities list</p>} />
+            <Route path="countries" element={<p>countries list</p>} />
+            <Route path="form" element={<p>form</p>} />
+          </Route>
 
-    </Routes>
-  </BrowserRouter>
-  
-  </div>
-  
-  )
-
-
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
