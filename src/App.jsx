8c1edd237660858a1db55195b60a238d452da4f0
lonelydomino <lonelydomino@icloud.com>
2025-08-27 21:43:41 -0700

    Update App.jsx with lazy loading components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { lazy, Suspense } from "react";
 
-import Product from "./pages/Product";
-import Pricing from "./pages/Pricing";
-import Homepage from "./pages/Homepage";
-import PageNotFound from "./pages/PageNotFound";
-import AppLayout from "./pages/AppLayout";
-import Login from "./pages/Login";
+import { CitiesProvider } from "./context/CitiesContext";
+import { AuthProvider } from "./context/FakeAuthContext";
+
+const Product = lazy() (() => import("./pages/Product"))
+const Pricing = lazy() (() => import("./pages/Pricing"))
+const Homepage = lazy() (() => import("./pages/Homepage"))
+const PageNotFound = lazy() (() => import("./pages/PageNotFound"))
+const AppLayout = lazy() (() => import("./pages/AppLayout"))
+const Login = lazy() (() => import("./pages/Login"))
+
+import SpinnerFullPage from './components/SpinnerFullPage'
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
-import { CitiesProvider } from "./context/CitiesContext";
-import { AuthProvider } from "./context/FakeAuthContext";
 import ProtectedRoute from './pages/ProtectedRoute'
 function App() {
   return (
@@ -19,6 +23,7 @@ function App() {
       <AuthProvider>
         <CitiesProvider>
           <BrowserRouter>
+          <Suspense fallback={<SpinnerFullPage />}>
             <Routes>
               <Route index element={<Homepage />} />
               <Route path="pricing" element={<Pricing />} />
@@ -38,6 +43,8 @@ function App() {
               <Route path="*" element={<PageNotFound />} />
               {/* Catches all other routes */}
             </Routes>
+
+          </Suspense>
           </BrowserRouter>
         </CitiesProvider>
       </AuthProvider>
